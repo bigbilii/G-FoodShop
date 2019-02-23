7cf9230a41a2f1cf462f0f28681aeaf2d5e8e11a
bigbilii <bigbilii@outlook.com>
2019-02-23 20:08:56 +0800

    禁止、恢复用户状态

--- gfoodshop-manager-service/src/main/java/me/guoxin/manager/service/UserService.java
+++ gfoodshop-manager-service/src/main/java/me/guoxin/manager/service/UserService.java
@@ -1,7 +1,6 @@
 package me.guoxin.manager.service;
 
 
-
 import me.guoxin.dto.DataTableViewPageDTO;
 import me.guoxin.dto.OnlineUserDTO;
 import me.guoxin.pojo.DataTableDTO;
@@ -14,6 +13,7 @@ public interface UserService {
 
     /**
      * 获取用户信息
+     *
      * @param userPhone 用户登录手机号
      * @return 用户信息
      */
@@ -21,6 +21,7 @@ public interface UserService {
 
     /**
      * 判断手机号是否存在
+     *
      * @param userPhone 用户登录手机
      * @return
      */
@@ -28,6 +29,7 @@ public interface UserService {
 
     /**
      * 根据用户手机号获取角色信息
+     *
      * @param userPhone
      * @return
      */
@@ -35,6 +37,7 @@ public interface UserService {
 
     /**
      * 根据用户手机号获取权限信息
+     *
      * @param userPhone
      * @return
      */
@@ -42,50 +45,58 @@ public interface UserService {
 
     /**
      * 查询用户信息
-     * @return
+     *
      * @param dataTableDTO
+     * @return
      */
     DataTableViewPageDTO<GfsUser> listUsers(DataTableDTO dataTableDTO);
 
     /**
      * 获取在线用户
+     *
      * @return
      */
     DataTableViewPageDTO<OnlineUserDTO> getOnlineUser(DataTableDTO dataTableDTO);
 
     /**
      * 踢出在线用户
+     *
      * @param sessionIds 会话id集合
      */
     void kickOutOnlineUser(List<String> sessionIds);
 
     /**
      * 删除用户
+     *
      * @param ids 用户id集合
      */
     void deleteUser(List<Long> ids);
 
     /**
      * 添加用户
+     *
      * @param gfsUser 用户信息
      */
     void insertUser(GfsUser gfsUser);
 
     /**
      * 修改用户
+     *
      * @param gfsUser 用户信息
      */
     void updateUser(GfsUser gfsUser);
 
     /**
      * 恢复用户状态
-     * @param id
+     *
+     * @param id 用户id
      */
     void restoreUser(Long id);
 
     /**
      * 禁止用户状态
-     * @param id
+     *
+     * @param id 用户id
      */
     void disableUser(Long id);
 }
